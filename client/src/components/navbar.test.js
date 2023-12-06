b6f68129d801dea8b6cf0d904a1ca9e2d2532c36
JakubKorytko <jakub.korytko@o2.pl>
2023-12-06 19:56:47 +0100

    Add client Navbar component unit tests

---
+++ client/src/components/navbar.test.js
@@ -0,0 +1,34 @@
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import Navbar from "./navbar";
+
+let container = null;
+
+beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+})
+
+afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+})
+
+test("back button visible when needed", () => {
+    act(() => {
+        render(<Navbar back={true} />, container)
+    });
+    
+    let backButtons = Array.from(container.querySelectorAll(".backButton"));
+    expect(backButtons.length).toBe(1)
+    
+    act(() => {
+        render(<Navbar back={false} />, container)
+    });
+
+    backButtons = Array.from(container.querySelectorAll(".backButton"));
+    expect(backButtons.length).toBe(0)
+
+})
