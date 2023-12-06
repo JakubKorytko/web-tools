936987c3531a22bd4ee5d78965b8023980f598e1
JakubKorytko <jakub.korytko@o2.pl>
2023-12-06 19:56:59 +0100

    Add client Token component unit tests

---
+++ client/src/components/token.test.js
@@ -0,0 +1,10 @@
+import Token from "./token";
+
+test("token works properly", () => {
+    const x = Math.random() * 100;
+    Token.value = x;
+    const bool1 = Token.value == x;
+    Token.remove();
+    const bool2 = bool1 && Token.value == undefined;
+    expect(bool2).toBe(true);
+})
