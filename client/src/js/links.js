c8f4b7f6ec31037b6be36e23681f6bdb5867528c
Jakub Korytko <jakub@korytko.me>
2023-12-21 14:19:21 +0100

    Remove unneeded files from client project

--- client/src/js/links.js
+++
@@ -1,8 +0,0 @@
-import ClipboardJS from "clipboard";
-import { Tooltip } from "bootstrap";
-
-new ClipboardJS('.bi-clipboard');
-const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new Tooltip(tooltipTriggerEl)
-})
