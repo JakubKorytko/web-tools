ae94347ac51e4e466e47dcaecae191fa7dfad79e
Jakub Korytko <jakub@korytko.me>
2023-12-22 15:46:42 +0100

    Fix server/pm2.json and lint client/.eslintrc.js

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,35 +1,35 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        "plugin:react/recommended",
-        "airbnb",
-    ],
-    parser: "@babel/eslint-parser",
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
-        ecmaVersion: 12,
-        sourceType: "module"
-    },
-    plugins: [
-        "react",
-    ],
-    rules: {
-        "sort-imports":
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'sort-imports':
         [
-            "error",
-            {
-                ignoreCase: true,
-                ignoreDeclarationSort: true
-            }
+          'error',
+          {
+            ignoreCase: true,
+            ignoreDeclarationSort: true,
+          },
         ],
-        "import/order": [ "error", {
-            groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
-            "newlines-between": "always"
-        }]
-    }
-}
+    'import/order': ['error', {
+      groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+      'newlines-between': 'always',
+    }],
+  },
+};
